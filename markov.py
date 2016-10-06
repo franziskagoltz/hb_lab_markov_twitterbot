c7b589e3ebf2c6f6f03c78377b9548e17d965661
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-05 18:12:06 -0700

    Added tweet function to tweet-markov chains

--- markov.py
+++ markov.py
@@ -3,6 +3,14 @@ import sys
 from random import choice
 import twitter
 
+api = twitter.Api(
+        consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+        consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+        access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+        access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+print api.VerifyCredentials()
+
 
 def open_and_read_file(filenames):
     """Given a list of files, open them, read the text, and return one long
@@ -45,7 +53,7 @@ def make_text(chains):
 
     key = choice(chains.keys())
     words = [key[0], key[1]]
-    while key in chains:
+    while key in chains and len(" ".join(words)) < 120:
         # Keep looping until we have a key that isn't in the chains
         # (which would mean it was the end of our original text)
         #
@@ -56,6 +64,7 @@ def make_text(chains):
         words.append(word)
         key = (key[1], word)
 
+    words.append('#hbgracefall16')
     return " ".join(words)
 
 
@@ -63,7 +72,9 @@ def tweet(chains):
     # Use Python os.environ to get at environmental variables
     # Note: you must run `source secrets.sh` before running this file
     # to make sure these environmental variables are set.
-    pass
+    status = api.PostUpdate(make_text(chains))
+    print status.text
+
 
 # Get the filenames from the user through a command line prompt, ex:
 # python markov.py green-eggs.txt shakespeare.txt
@@ -76,4 +87,5 @@ text = open_and_read_file(filenames)
 chains = make_chains(text)
 
 # Your task is to write a new function tweet, that will take chains as input
-# tweet(chains)
+tweet(chains)
+
